54a16eff209d26ffdfc16305acd59062be72f1b8
animesh <animeshs@mindfiresolutions.com>
2019-09-18 19:35:04 +0530

    fixed the ajax call function error

--- public/js/js1.js
+++ public/js/js1.js
@@ -1,27 +1,35 @@
-function ajaxcall(url, val) {
+function ajaxcall(url, val, callback) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			return this.responseText;
+			callback(this.responseText);
+				console.log('5');
+
 		}
 	}
-	xmlhttp.open("GET" , "response1.php?a="+val, true);
+	xmlhttp.open("GET" , url+"?a="+val, true);
+		console.log('6');
+
 	xmlhttp.send();
+		console.log('7');
+
 }
-
-
-
-
-
-
-
 
 function bookpopup(str1) {
-	var obj1= ajaxcall("response1.php", str1);
-	alert(obj1);
-	alert("ccccccc");
+	console.log('1');
+	ajaxcall("response1.php", str1, showalert);
+	console.log('2');
+}
+
+console.log('3');
+
+function showalert($obj1) {
+	console.log('4');
+	$obj1 = JSON.parse($obj1);
 	alert("Title: "+$obj1.title+" ISBN: "+$obj1.book_isbn+" Selling Since: "+$obj1.created_at);
 }
+	console.log('8');
+
 
 
 	    
