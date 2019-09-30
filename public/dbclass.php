35bf8895dc9549e65de6659546fc809bfb09d43c
animesh <animeshs@mindfiresolutions.com>
2019-09-30 19:06:21 +0530

    db custom class

--- public/dbclass.php
+++ public/dbclass.php
@@ -15,6 +15,20 @@
 			return self::$connection;
 		}
 
+		function select($select_str) {
+			$select= "*";
+			if (!empty($select_str)) {
+				$select= $select_str;
+			$conn= self::getInstance("localhost:3306", "root", "mindfire", "bookworm");
+			$conn= self::$dbconn;
+			$stmt= $conn->prepare("SELECT ".$select_str." from ".$this->tablename);
+			$stmt->bind_param("ss", $select_str, $this->tablename);
+			$stmt->execute();
+			$result=$stmt->get_result();
+			echo $result->fetch_assoc();
+			}
+		}
+
 
 		/** 
    		* Method to get all the records from a table
@@ -22,7 +36,7 @@
    		* @returns result object.
  		*/
 
-		public function viewall($tablename) {
+		/*public function viewall($tablename) {
 
 			$conn = static::$dbconn;
 
@@ -48,24 +62,65 @@
 			$select = $select." ".$this;
 
 			return $select;
-		}
-
-		
+		}*/		
 	}
+
+	class Book extends Database {
+		var $tablename= 'book';
+
+
+		function __construct() {
+			;
+		}
+	}
+
+	$book1= new Book();
+	$book1->select("id, title");
 
 
 
 	/**
 	 * 
 	 */
-	class Select extends Database
-	{
-		
-		function __construct(argument)
-		{
-			
+	
+
+
+	class Person {
+		var $name;
+		var $gender;
+		var $age;
+
+		function __construct($name, $gender, $age) {
+			$this->name= $name;
+			$this->gender= $gender;
+			$this->age= $age;
+		}
+
+		function person_details() {
+			echo $this->name.", ".$this->gender.", is ".$this->age."<br>";
 		}
 	}
+
+
+	class Employee extends Person {
+		var $dept;
+		function __construct($name, $gender, $age, $dept) {
+			parent::__construct($name, $gender, $age);
+			$this->dept = $dept;
+		}
+
+		function employee_details() {
+			echo $this->name.", ".$this->gender.", is ".$this->age." and works in ".$this->dept."<br>";
+		}
+	}
+
+	$person1= new Person("Animesh", "Male", "22");
+	$employee1= new Employee("XYZ", "Female", "21", "IT");
+	$employee2= new Employee("ABCD", "Male", "25", "HR");
+
+	$person1->person_details();
+	$employee2->person_details();
+	$employee1->employee_details();
 
 	
 
